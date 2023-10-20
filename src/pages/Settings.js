8852746d5e39373ca2f147f2f6505d796ef3eee3
Dhinki2002 <102169998+Dhinki2002@users.noreply.github.com>
2023-10-20 15:37:38 +0530

    Settings done

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -4,33 +4,68 @@ import Header from "../components/Header";
 import SettingsPane from "../components/SettingsPane";
 import { useNavigate } from "react-router-dom";
 import "./Settings.css";
+import { useState } from "react";
 
 const Settings = () => {
-  const counter=4;
-  const settingsCounter=0;
+  const counter = 4;
+  const settingsCounter = 0;
+
+  const [name, setName] = useState("Priyanshu Gupta");
+  const [email, setEmail] = useState("87priyanshugupta@gmail.com");
+  const [phoneNumber, setPhoneNumber] = useState("7727042145");
+  const [collegeName, setcollegeName] = useState("Indian Institute of Technology");
+  const [year, setYear] = useState("2025");
+  const [targetRoles, setTargetRoles] = useState("SDE-1");
+  const [editMode, setEditMode] = useState(false);
+
+  const handleEditClick = () => {
+    setEditMode(!editMode);
+  };
 
   return (
     <div className="settings5">
-      <Navbar counter={counter}/>
+      <Navbar counter={counter} />
       <Header />
       <div className="rectangle-parent4">
-        <SettingsPane settingsCounter={settingsCounter}/>
+        <SettingsPane settingsCounter={settingsCounter} />
         <div className="my-profile7">My Profile</div>
         <div className="frame-parent196">
           <div className="frame-parent197">
             <div className="email-address-container">
               <div className="college-name">Name</div>
-              <div className="indian-institute-of">Priyanshu Gupta</div>
+              {editMode ? (
+                <input
+                  className="indian-institute-of"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              ) : (
+                <div className="indian-institute-of">{name}</div>
+              )}
             </div>
             <div className="email-address-container">
               <div className="college-name">Email address</div>
-              <div className="indian-institute-of">
-                87priyanshugupta@gmail.com
-              </div>
+              {editMode ? (
+                <input
+                  className="indian-institute-of"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              ) : (
+                <div className="indian-institute-of">{email}</div>
+              )}
             </div>
             <div className="email-address-container">
               <div className="college-name">Phone number</div>
-              <div className="indian-institute-of">7727042145</div>
+              {editMode ? (
+                <input
+                  className="indian-institute-of"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+              ) : (
+                <div className="indian-institute-of">{phoneNumber}</div>
+              )}
             </div>
             <div className="frame-parent198">
               <div className="password-parent1">
@@ -48,20 +83,34 @@ const Settings = () => {
             </div>
             <div className="email-address-container">
               <div className="college-name">College Name</div>
-              <div className="indian-institute-of">
-                Indian Institute of technology, Roorkee
-              </div>
+              {editMode ? (
+                <input
+                  className="indian-institute-of"
+                  value={collegeName}
+                  onChange={(e) => setcollegeName(e.target.value)}
+                />
+              ) : (
+                <div className="indian-institute-of">{collegeName}</div>
+              )}
             </div>
           </div>
           <div className="frame-parent197">
             <div className="email-address-container">
               <div className="college-name">Graduation year</div>
-              <div className="indian-institute-of">2025</div>
+              {editMode ? (
+                <input
+                  className="indian-institute-of"
+                  value={year}
+                  onChange={(e) => setYear(e.target.value)}
+                />
+              ) : (
+                <div className="indian-institute-of">{year}</div>
+              )}
             </div>
             <div className="skills-container">
               <div className="college-name">Skills</div>
-              <div className="frame-parent200">
-                <div className="machine-learning-wrapper1">
+              <div className="frame-parent200"> 
+                <div className="machine-learning-wrapper1"> 
                   <div className="machine-learning5">Machine learning</div>
                 </div>
                 <div className="machine-learning-wrapper1">
@@ -74,7 +123,15 @@ const Settings = () => {
             </div>
             <div className="email-address-container">
               <div className="college-name">Target Role</div>
-              <div className="indian-institute-of">SDE-I</div>
+              {editMode ? (
+                <input
+                  className="indian-institute-of"
+                  value={targetRoles}
+                  onChange={(e) => setTargetRoles(e.target.value)}
+                />
+              ) : (
+                <div className="indian-institute-of">{targetRoles}</div>
+              )}
             </div>
             <div className="frame-parent201">
               <div className="email-address-container">
@@ -89,7 +146,7 @@ const Settings = () => {
             </div>
           </div>
         </div>
-        <button className="frame-wrapper80">
+        <button className="frame-wrapper80" onClick={handleEditClick}>
           <div className="tableredit-container">
             <img className="tableredit-icon2" alt="" src="/tableredit1.svg" />
             <div className="edit2">Edit</div>
