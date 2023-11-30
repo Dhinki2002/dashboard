1db32a600ee0e6963081a119d7a117eaea151ab8
Dhinki2002 <102169998+Dhinki2002@users.noreply.github.com>
2023-11-30 13:45:07 +0530

    boilerplate added

--- src/components/Coding/Coding.js
+++ src/components/Coding/Coding.js
@@ -25,20 +25,46 @@ const Coding = ({ currentQuestion, currentStep, onNextClick, onPrevClick }) => {
   const [code, setCode] = useState(
     Array.from({ length: 25 }, () => "").join("\n")
   );
+  const [userCode, setUserCode] = useState("int main() {\n//write your  Cpp code here\n\n\n\n\n \n\n\n\n\n\n\nreturn 0;\n}");
+
+  const handleLanguageChange = (e) => {
+    const selectedLanguage = e.target.value;
+    let boilerplateCode = "";
+
+    switch (selectedLanguage) {
+      case "c_cpp":
+        boilerplateCode = "int main() {\n//write your  Cpp code here\n\n\n\n\n \n\n\n\n\n\n\nreturn 0;\n}";
+        break;
+      case "python":
+        boilerplateCode = "def main(): \n//write your python code here\n\n\n\n\n \n\n\n\n\n\n\n\n";
+        break;
+      case "java":
+        boilerplateCode = "int main() {\n//write your java code here\n\n\n\n\n \n\n\n\n\n\n\nreturn 0;\n}";
+        break;
+      case "c":
+        boilerplateCode = "int main() {\n//write your C code here\n\n\n\n\n \n\n\n\n\n\n\nreturn 0;\n}";
+        break;
+      default:
+        boilerplateCode = "";
+    }
+
+    setUserCode(boilerplateCode);
+  };
+
 
   return (
     <>
       <div className="frame-parent257">
 
         <div className="frame-parent258">
-          <select className="frame-child189" onChange={(e) => setMode(e.target.value)}>
+          <select className="frame-child189" onChange={handleLanguageChange}>
             <option value="c_cpp">C++</option>
             <option value="python">Python</option>
             <option value="java">Java</option>
             <option value="c">C</option>
           </select>
           <div className="icoutline-refresh-parent">
-            <button className="icoutline-refresh" onClick={() => setCode("#1 Write your code here")}>
+            <button className="icoutline-refresh" onClick={() => setUserCode("int main() {\n//write your  Cpp code here\n\n\n\n\n \n\n\n\n\n\n\nreturn 0;\n}")}>
               <img className="vector-icon40" alt="" src="/vector26.svg" />
             </button>
             <button className="icoutline-refresh">
@@ -47,25 +73,25 @@ const Coding = ({ currentQuestion, currentStep, onNextClick, onPrevClick }) => {
           </div>
         </div>
         <div className="test">
-        <CodeMirror
-          mode="c_cpp"
-          theme={vscodeDark}
-          value={code}
-          onChange={(newCode) => setCode(newCode)}
-          name="code-editor"
-          editorProps={{ $blockScrolling: true }}
-          options={{ viewportMargin: 10 }}
-          wrapperClassName="custom-code-mirror-wrapper" 
-          style={{
-            height: "30vw", // Change the height value here
-            width: "46vw",
-            maxWidth: "60vw",
-            maxHeight: "80vw",
-            borderRadius: "8px" ,
-            fontSize: "20px",
-            
-          }}
-        />
+          <CodeMirror
+            mode="c_cpp"
+            theme={vscodeDark}
+            value={userCode} // Set the initial user code here
+            onChange={(newCode) => setUserCode(newCode)}
+            name="code-editor"
+            editorProps={{ $blockScrolling: true }}
+            options={{ viewportMargin: 10 }}
+            wrapperClassName="custom-code-mirror-wrapper"
+            style={{
+              height: "30vw", // Change the height value here
+              width: "46vw",
+              maxWidth: "60vw",
+              maxHeight: "80vw",
+              borderRadius: "8px",
+              fontSize: "20px",
+
+            }}
+          />
         </div>
 
 
