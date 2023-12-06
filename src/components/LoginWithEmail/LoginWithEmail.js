0a962566541a73f702b3606c9ef4467f64142cf6
Dhinki2002 <102169998+Dhinki2002@users.noreply.github.com>
2023-12-06 16:33:42 +0530

    login_email

--- src/components/LoginWithEmail/LoginWithEmail.js
+++ src/components/LoginWithEmail/LoginWithEmail.js
@@ -37,8 +37,7 @@ const LoginWithEmail = () => {
     
 
     
-    navigate('/go-score-dashboardindividual');
-    // navigate('/go-score-dashboardindividual');
+    navigate('/dashboard');
   }, [navigate]);
 
 
@@ -52,15 +51,15 @@ const LoginWithEmail = () => {
 
   return (
    
-    <div className="relative bg-[color:var(--color-white-login)] w-full h-[56.12vw] overflow-hidden text-left text-[length:var(--font-size-base-login)] text-[color:var(--color-gray-100-login)]; font-family: var(--h1-heavy-login)">
-      <div className="flex flex-col items-center justify-start gap-[var(--gap-37xl-login)] items-start gap-[var(--gap-36xl-login)] absolute gap-[var(--gap-41xl-login)] left-[4.8vw] top-[5.38vw]">
+    <div className="login-with-email1-login">
+      <div className="cantileverlogoblack-2-container-login">
         <CantileverLogo
           cantileverlogoblack2="/1644088580619-cantileverlogoblack-2@2x.png"
           cantileverlogoblack2IconPosition="relative"
           cantileverlogoblack2IconTop="unset"
           cantileverlogoblack2IconLeft="unset"
         />
-        <div className="flex flex-col items-center justify-start gap-[var(--gap-37xl-login)] items-start gap-[var(--gap-36xl-login)]">
+        <div className="frame-parent27-login">
           <WelcomeText
             frameDivPosition="unset"
             frameDivTop="unset"
@@ -69,12 +68,12 @@ const LoginWithEmail = () => {
             yourPathToColor="#212121"
             welcomeToFulcrum="Welcome to Fulcrum"
           />
-          <div className="flex flex-col items-center justify-start gap-[var(--gap-37xl-login)]">
-            <div className="flex flex-col items-start justify-start gap-[var(--gap-13xl-login)]">
-              <div className="flex flex-col items-start justify-start gap-[var(--gap-13xl-login)]">
-                <div className="flex flex-row items-start justify-start gap-[var(--gap-9xl-login)]">
-                  <div className="flex flex-row justify-center cursor-pointer border-[color:var(--color-gainsboro-300-login)] pt-[var(--padding-base-login)] pr-[var(--padding-5xl-login)] pb-[var(--padding-base-login)] pl-[var(--padding-5xl-login)] bg-transparent rounded-[var(--br-xs-login)] items-center border-[0.06vw] border-solid">
-                  <div className="flex flex-row justify-center items-end gap-[var(--gap-mini-login)]">
+          <div className="frame-parent28-login">
+            <div className="frame-parent29-login">
+              <div className="frame-parent29-login">
+                <div className="frame-parent31-login">
+                  <div className="frame-wrapper19-login">
+                  <div className="flat-color-iconsgoogle-container-login">
                     <GoogleLogin
                           clientId={clientId}
                           buttonText="Sign up with Google"
@@ -86,50 +85,50 @@ const LoginWithEmail = () => {
                         </div>
                         </div>
                   <button
-                    className="cursor-pointer border-[color:var(--color-gainsboro-300-login)] pt-[var(--padding-base-login)] pr-[var(--padding-5xl-login)] pb-[var(--padding-base-login)] pl-[var(--padding-5xl-login)] bg-transparent rounded-[var(--br-xs-login)] box-border w-[14.8vw] flex flex-row items-center justify-center border-[0.06vw] border-solid"
+                    className="frame-wrapper20-login"
                     onClick={onFrameButton1Click}
                   >
-                    <div className="flex flex-row justify-center items-end gap-[var(--gap-mini-login)]">
+                    <div className="flat-color-iconsgoogle-container-login">
                       <img
-                        className="relative w-[1.6vw] h-[1.6vw] overflow-hidden shrink-0"
+                        className="mingcutecoupon-line-icon-login"
                         alt=""
                         src="/mingcutecouponline.svg"
                       />
-                      <div className="relative text-[length:var(--body-heavy-size-login)] text-[color:var(--black-login)] text-left; font-family: var(--h1-heavy-login)">Login with Code</div>
+                      <div className="login-with-google1-login">Login with Code</div>
                     </div>
                   </button>
                 </div>
-                <div className="flex-row items-center gap-[var(--gap-7xs-login)] flex justify-start">
-                  <div className="flex-row items-center gap-[var(--gap-7xs-login)] flex justify-start">
-                    <div className="border-t-[color:var(--color-gainsboro-400-login)] box-border w-[14.86vw] h-[0.06vw] border-t-[0.06vw] border-solid relative" />
-                    <div className="relative">or</div>
+                <div className="line-container-login">
+                  <div className="line-container-login">
+                    <div className="frame-child14-login" />
+                    <div className="or2-login">or</div>
                   </div>
-                  <div className="border-t-[color:var(--color-gainsboro-400-login)] box-border w-[14.86vw] h-[0.06vw] border-t-[0.06vw] border-solid relative" />
+                  <div className="frame-child14-login" />
                 </div>
               </div>
-              <div className="flex flex-col items-start justify-start gap-[2.28vw] text-[length:var(--body-heavy-size-login)] text-[color:var(--black-login)]">
-                <div className="flex flex-col items-start justify-start gap-[var(--gap-base-login)] gap-[var(--gap-7xl-login)]">
+              <div className="frame-parent33-login">
+                <div className="frame-parent34-login">
 
-                  <div className="flex justify-start flex-col items-start gap-[var(--gap-5xs-login)]">
-                    <div className="relative">Email</div>
+                  <div className="email-group-login">
+                    <div className="or2-login">Email</div>
                     <input
-                      className="flex justify-start border-[color:var(--color-gainsboro-300-login)] text-[length:var(--body-heavy-size-login)] bg-transparent rounded-[var(--br-xs-login)] box-border w-[31.2vw] flex-row items-center pt-[var(--padding-base-login)] pr-[var(--padding-5xl-login)] pb-[var(--padding-base-login)] pl-[var(--padding-5xl-login)] border-[0.06vw] border-solid; font-family: var(--h1-heavy-login)"
+                      className="frame-child16-login"
                       placeholder="Enter Email"
                       type="email"
                     />
                   </div>
-                  <div className="flex flex-col items-start justify-start gap-[var(--gap-base-login)]">
-                    <div className="flex justify-start flex-col items-start gap-[var(--gap-5xs-login)]">
-                      <div className="relative">Password</div>
+                  <div className="frame-parent35-login">
+                    <div className="email-group-login">
+                      <div className="or2-login">Password</div>
                       <input
-                        className="border-[color:var(--color-gainsboro-300-login)] text-[length:var(--body-heavy-size-login)] bg-transparent rounded-[var(--br-xs-login)] box-border w-[31.2vw] flex flex-row items-center justify-between pt-[var(--padding-base-login)] pr-[var(--padding-5xl-login)] pb-[var(--padding-base-login)] pl-[var(--padding-5xl-login)] border-[0.06vw] border-solid; font-family: var(--h1-heavy-login)"
+                        className="frame-child17-login"
                         placeholder="Enter your password"
                         type="password"
                       />
                     </div>
 
                     <Link
-                      className="cursor-pointer no-underline relative text-[length:var(--font-size-base-login)] text-[color:var(--purple-login)]"
+                      className="forgot-password-login"
                       to="/forgot-reset"
                     >
                       Forgot password?
@@ -139,16 +138,16 @@ const LoginWithEmail = () => {
 
 
                 </div>
-                <button className="flex flex-row items-end justify-center cursor-pointer pt-[var(--padding-xl-login)] pr-[var(--padding-5xl-login)] pb-[var(--padding-xl-login)] pl-[var(--padding-5xl-login)] bg-[color:var(--purple-login)] rounded-[var(--br-xs-login)] w-[31.2vw] items-center justify-between box-border border-0" onClick={onFrameButtonClick}>
-                  <div className="flex flex-row items-end justify-center">
-                    <div className="relative text-[length:var(--body-heavy-size-login)] text-[color:var(--color-white-login)] text-left; font-family: var(--h1-heavy-login)">Login</div>
+                <button className="frame-wrapper21-login" onClick={onFrameButtonClick}>
+                  <div className="login-container-login">
+                    <div className="login1-login">Login</div>
                   </div>
                 </button>
               </div>
             </div>
-            <Link className="cursor-pointer no-underline relative inline-block w-[14.5vw] text-[color:var(--black-login)]" to="/">
+            <Link className="new-to-fulcrum-container2-login" to="/">
               <span>{`New to Fulcrum? `}</span>
-              <span className="text-[color:var(--purple-login)]">Sign Up for free</span>
+              <span className="sign-up-for2-login">Sign Up for free</span>
             </Link>
           </div>
         </div>
